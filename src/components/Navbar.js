f8be76a0eba3ed5fcbcd5109d84d41b09cb4a144
Alexander Roldan <alexroldan49@gmail.com>
2022-01-27 01:32:08 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import react from "react";
+
+
+function Navbar(){
+
+    return(
+       <div className="navbar">
+           <ul className="nav-list" >
+               <li className="home-button">Home</li>
+               <li className="home-button" >about me</li>
+               <li className="home-button" >My Apps</li>
+           </ul>
+       </div>
+    )
+}
+
+export default Navbar
